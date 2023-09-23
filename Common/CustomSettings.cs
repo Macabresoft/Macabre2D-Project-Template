dcc4d932b1695999b6ecd473b2b6378375a9e783
Brett Story <brettmstory@gmail.com>
2023-09-23 15:34:55 -0500

    Adding CustomSettings

---
+++ Common/CustomSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+using System.Runtime.Serialization;
+
+/// <summary>
+/// Settings custom to this specific project.
+/// </summary>
+[DataContract]
+public class CustomSettings {
+    /// <summary>
+    /// Gets or sets the author of this project.
+    /// </summary>
+    /// <remarks>
+    /// This is an example setting and may be removed without consequence.
+    /// </remarks>
+    [DataMember]
+    public string Author { get; set; }
+}
