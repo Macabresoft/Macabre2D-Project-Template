81a7ecf8a1ecf8636d6493fb1e87b374dfb59ffc
Brett Story <brettmstory@gmail.com>
2023-10-22 12:44:48 -0500

    Clone and CopyTo methods added to CustomSettings default

--- Common/CustomSettings.cs
+++ Common/CustomSettings.cs
@@ -8,11 +8,31 @@ using System.Runtime.Serialization;
 [DataContract]
 public class CustomSettings {
     /// <summary>
-    /// Gets or sets the author of this project.
+    /// Gets or sets the user who owns these settings.
     /// </summary>
     /// <remarks>
     /// This is an example setting and may be removed without consequence.
     /// </remarks>
     [DataMember]
-    public string Author { get; set; }
+    public string UserName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Clones this instance.
+    /// </summary>
+    /// <remarks>This method is required by Macabre2D.</remarks>
+    /// <returns>The cloned settings.</returns>
+    public CustomSettings Clone() {
+        return new CustomSettings {
+            UserName = this.UserName
+        };
+    }
+
+    /// <summary>
+    /// Copies settings to another instance.
+    /// </summary>
+    /// <remarks>This method is required by Macabre2D.</remarks>
+    /// <param name="other">The other instance.</param>
+    public void CopyTo(CustomSettings other) {
+        other.UserName = this.UserName;
+    }
 }
