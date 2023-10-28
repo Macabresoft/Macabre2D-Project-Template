0d2303985fe0e5867f04ab242b7cb350c0f471fd
Brett Story <brettmstory@gmail.com>
2023-10-28 09:30:32 -0500

    VolumeCategory added to template

---
+++ Common/VolumeCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+/// <summary>
+/// Pre-defined audio categories that can be adjusted independently.
+/// </summary>
+public enum VolumeCategory {
+    Default,
+    Overall,
+    Effect,
+    Menu,
+    Music,
+    Notification,
+    Voice
+}
