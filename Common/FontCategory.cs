98acdf830bdc60320c15bbfac0de9d2cd724b2d6
Brett Story <brettmstory@gmail.com>
2024-06-02 09:30:40 -0500

    Project fonts

---
+++ Common/FontCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+/// <summary>
+/// Defines a font category that can be associated with a font elsewhere.
+/// </summary>
+public enum FontCategory : byte {
+    None
+}
---
+++ Common/ResourceCulture.cs
@@ -0,0 +1,37 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+/// <summary>
+/// Defines a culture to bse used by <see cref="Resources" /> (Resources.resx).
+/// </summary>
+public enum ResourceCulture {
+    Default = 0
+}
+
+/// <summary>
+/// Extensions for <see cref="ResourceCulture" />.
+/// </summary>
+public static class ResourceCultureExtensions {
+    private const string DefaultCultureName = "default";
+
+    /// <summary>
+    /// Converts a <see cref="ResourceCulture" /> into the culture name as used by <see cref="Resources" /> (Resources.resx).
+    /// </summary>
+    /// <param name="culture">The culture.</param>
+    /// <returns>The culture's name.</returns>
+    public static string ToCultureName(this ResourceCulture culture) {
+        return culture switch {
+            _ => "default"
+        };
+    }
+
+    /// <summary>
+    /// Converts a <see cref="ResourceCulture" /> into the culture name as used by <see cref="Resources" /> (Resources.resx).
+    /// </summary>
+    /// <param name="culture">The culture.</param>
+    /// <returns>The culture's display name.</returns>
+    public static string ToDisplayName(this ResourceCulture culture) {
+        return culture switch {
+            _ => "English (US)"
+        };
+    }
+}
