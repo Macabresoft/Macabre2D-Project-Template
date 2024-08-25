51c7b02af02c96a76015db5b93b53d66d6b4ee72
Brett Story <brettmstory@gmail.com>
2024-08-25 14:39:27 -0500

    GameState and MonoGame upgrade issues

---
+++ Common/GameState.cs
@@ -0,0 +1,7 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+/// <summary>
+/// A class accessible via the containing game that can include any information about the currently running game that might be relevant in the current scene or across scenes.
+/// </summary>
+public class GameState {
+}
