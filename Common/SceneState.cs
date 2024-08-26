1db062884c62087dacbb07cecc3589b62cf03687
Brett Story <brettmstory@gmail.com>
2024-08-26 16:03:02 -0500

    Scene State

---
+++ Common/SceneState.cs
@@ -0,0 +1,16 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+using System.Runtime.Serialization;
+
+/// <summary>
+/// Then state of the scene.
+/// </summary>
+[DataContract]
+public class SceneState {
+    /// <summary>
+    /// Initializes this instance with the game state.
+    /// </summary>
+    /// <param name="gameState">The game state.</param>
+    public void Initialize(GameState gameState) {
+    }
+}
