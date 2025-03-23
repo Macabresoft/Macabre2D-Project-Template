718640bffddd0bb0e415bbc0c95f45a69f0730b7
Brett Story <brettmstory@gmail.com>
2025-03-23 16:09:38 -0500

    Removing action requested

--- Common/GameState.cs
+++ Common/GameState.cs
@@ -4,24 +4,10 @@ using Macabresoft.Core;
 using Macabresoft.Macabre2D.Common;
 
 /// <summary>
-/// Events that can be raised to the game.
-/// </summary>
-public enum GameAction {
-    Shutdown,
-    SaveSettings,
-    SaveAndApplySettings
-}
-
-/// <summary>
 /// A class accessible via the containing game that can include any information about the currently running game that might be relevant in the current scene or across scenes.
 /// </summary>
 public class GameState {
     private IDataManager _dataManager = EmptyDataManager.Instance;
-
-    /// <summary>
-    /// An event that is called when it is requested that the game should perform a specific action.
-    /// </summary>
-    public event EventHandler<GameAction>? ActionRequested;
 
     /// <summary>
     /// Initializes this instance.
@@ -30,13 +16,5 @@ public class GameState {
     /// <param name="customSettings">The custom settings.</param>
     public void Initialize(IDataManager dataManager, CustomSettings customSettings) {
         this._dataManager = dataManager;
-    }
-
-    /// <summary>
-    /// Raises an event which requests an action be taken by the game.
-    /// </summary>
-    /// <param name="action">The action.</param>
-    public void RaiseActionRequested(GameAction action) {
-        this.ActionRequested.SafeInvoke(this, action);
     }
 }
