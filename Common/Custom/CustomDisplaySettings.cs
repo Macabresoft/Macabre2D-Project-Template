d9f0218bb53f56845ae81352d98804f551858c24
macabrett <brett@macabresoft.com>
2025-09-12 16:53:15 -0500

    Upgrading NuGet packages, adding CustomDisplaySettings to template, and adding RenderPriority to template.

---
+++ Common/Custom/CustomDisplaySettings.cs
@@ -0,0 +1,28 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+using System.Runtime.Serialization;
+
+/// <summary>
+/// Display settings custom to this specific project.
+/// </summary>
+[DataContract]
+public class CustomDisplaySettings {
+    /// <summary>
+    /// Clones this instance.
+    /// </summary>
+    /// <remarks>This method is required by Macabre2D.</remarks>
+    /// <returns>The cloned settings.</returns>
+    public CustomDisplaySettings Clone() {
+        var clone = new CustomDisplaySettings();
+        this.CopyTo(clone);
+        return clone;
+    }
+
+    /// <summary>
+    /// Copies settings to another instance.
+    /// </summary>
+    /// <remarks>This method is required by Macabre2D.</remarks>
+    /// <param name="other">The other instance.</param>
+    public void CopyTo(CustomDisplaySettings other) {
+    }
+}
---
+++ Common/RenderPriority.cs
@@ -0,0 +1,9 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+/// <summary>
+/// The layers to be used when determining the render order of renderable entities.
+/// </summary>
+public enum RenderPriority : byte {
+    Default = 0,
+    Final = byte.MaxValue
+}
