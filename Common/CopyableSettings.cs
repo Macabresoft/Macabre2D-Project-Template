458a23b1adb8d26365c8320e394335fe163d022a
macabrett <brett@macabresoft.com>
2025-09-14 14:54:51 -0500

    CopyableSettings and CustomInputSettings added to default template

---
+++ Common/CopyableSettings.cs
@@ -0,0 +1,29 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+using System.Runtime.Serialization;
+using Macabresoft.Macabre2D.Common;
+
+/// <summary>
+/// Represents a settings file that can be copied.
+/// </summary>
+/// <typeparam name="T">The type of settings that can be copied and cloned.</typeparam>
+[DataContract]
+public abstract class CopyableSettings<T> : VersionedData where T : CopyableSettings<T>, new() {
+    /// <summary>
+    /// Clones this instance.
+    /// </summary>
+    /// <remarks>This method is required by Macabre2D.</remarks>
+    /// <returns>The cloned settings.</returns>
+    public virtual T Clone() {
+        var clone = new T();
+        this.CopyTo(clone);
+        return clone;
+    }
+
+    /// <summary>
+    /// Copies settings to another instance.
+    /// </summary>
+    /// <remarks>This method is required by Macabre2D.</remarks>
+    /// <param name="other">The other instance.</param>
+    public abstract void CopyTo(T other);
+}
---
+++ Common/Custom/CustomInputSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Macabresoft.Macabre2D.Project.Common;
+
+using System.Runtime.Serialization;
+
+/// <summary>
+/// Display settings custom to this specific project.
+/// </summary>
+[DataContract]
+public class CustomInputSettings : CopyableSettings<CustomInputSettings> {
+    /// <inheritdoc />
+    public override void CopyTo(CustomInputSettings other) {
+    }
+}
